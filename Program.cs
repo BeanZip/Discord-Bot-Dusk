44c3da06e60b16324694959cc68fe0a7f02c7f95
Ichi <78321273+BeanZip@users.noreply.github.com>
2025-03-01 13:15:59 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Discord_Bot_Dusk;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+    public static async Task Main()
+    {
+      _client = new DiscordSocketClient();
+      _client.Log += Log;
+      
+      var token = Environment.GetEnvironmentVariable("BotToken");
+      await _client.LoginAsync(TokenType.Bot, token);
+      await _client.StartAsync();
+      await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
