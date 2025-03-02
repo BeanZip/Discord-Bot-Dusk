b4ca6a70187c9365bcbdc9e9a55ea638b22f8ecb
Ichi <78321273+BeanZip@users.noreply.github.com>
2025-03-02 18:54:38 -0500

    Fixed up bug

--- Program.cs
+++ Program.cs
@@ -135,7 +135,7 @@ public class Program
                 if(command.User.Id != ulong.Parse(Environment.GetEnvironmentVariable("FatherId")))
                 {
                     await command.RespondAsync("# 💥 KABOOM 💥 #");
-                    await command.RespondAsync($"{command.User.Mention} has blew himself up");
+                    await command.FollowupAsync($"{command.User.Mention} has blew himself up");
                 } else{
                     await command.RespondAsync("I'm sorry, I can't let you do that father.");
                     await Task.Delay(2000);
